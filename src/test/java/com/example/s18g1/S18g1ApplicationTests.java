2eb88e40c6fd9ffafacc4e43d639846d3b015b7e
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-23 01:28:23 +0300

    konfigurasyonlar yapildi

---
+++ src/test/java/com/example/s18g1/S18g1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s18g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18g1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
