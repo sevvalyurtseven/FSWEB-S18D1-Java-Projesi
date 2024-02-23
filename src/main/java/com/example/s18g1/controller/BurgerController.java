9a86f3fb5880b64ef4b518ed1a6c56654f5b4064
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-23 04:56:07 +0300

    controller katmaninda CRUD islemleri olusturuldu

--- src/main/java/com/example/s18g1/controller/BurgerController.java
+++ src/main/java/com/example/s18g1/controller/BurgerController.java
@@ -1,10 +1,13 @@
 package com.example.s18g1.controller;
 
 import com.example.s18g1.dao.BurgerDao;
+import com.example.s18g1.entity.BreadType;
+import com.example.s18g1.entity.Burger;
+import com.example.s18g1.util.BurgerValidation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -14,5 +17,44 @@ public class BurgerController {
     @Autowired
     public BurgerController(BurgerDao burgerDao){
         this.burgerDao = burgerDao;
+    }
+    @PostMapping
+    public Burger save(@RequestBody Burger burger){
+        BurgerValidation.checkName(burger.getName());
+        return burgerDao.save(burger);
+    }
+    @GetMapping
+    public List<Burger> findAll(){
+        return burgerDao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Burger get(@PathVariable("id") long id){
+        return burgerDao.findById(id);
+    }
+
+    @PutMapping
+    public Burger update(@RequestBody Burger burger){
+        return burgerDao.update(burger);
+    }
+
+    @DeleteMapping("/{id}")
+    public Burger remove(@PathVariable long id){
+        return burgerDao.remove(id);
+    }
+    @GetMapping("/breadType/{breadType}")
+    public List<Burger> findByBreadType(@PathVariable String breadType){
+        BreadType bt = BreadType.valueOf(breadType);
+        return burgerDao.findByBreadType(bt);
+    }
+
+    @GetMapping("/price/{price}")
+    public List<Burger> findByPrice(@PathVariable Integer price){
+        return burgerDao.findByPrice(price);
+    }
+
+    @GetMapping("/content/{content}")
+    public List<Burger> findByContent(@PathVariable String content){
+        return burgerDao.findByContent(content);
     }
 }
